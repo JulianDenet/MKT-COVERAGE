17cebe62a7c678b869881d5051b2495968d9d298
JulianDenet <53799663+JulianDenet@users.noreply.github.com>
2021-08-13 23:30:42 -0500

    Update mktcov.py
    Exports more detailed output in a csv file

--- mktcov.py
+++ mktcov.py
@@ -1,65 +1,177 @@
 import openpyxl as oxl
-from gurobipy import *
+import pulp as plp
+import timeit
+import pandas as pd
 
-# Constants
-DRIVERS = 105
-COURSES = 148
+start = timeit.default_timer()
 
-# open the data file
-wb = oxl.load_workbook('input.xlsx')
-ws = wb['raw_matrix']
+###### PROBLEM PARAMETERS
 
-m = Model()
-m.setParam('OutputFlag', 0)
+TARGET_LEVEL_7 = True
+HIGH_ENDS_ONLY = True
+ONLY_USE_ADQUIRED_DRIVERS = False
+MINIMUM_DRIVERS_PER_COURSE = 2
+CONSIDER_CURRENT_INVESTMENT_LEVELS = True
+REQUIRE_PRIORITY_DRIVERS = True
+MINIMUM_PRIORITY_DRIVERS_PER_COURSE = 2
+UNAQUIRED_DRIVERS_COST = 2 # Measured in tickets
+PRIORITY_DRIVERS = ['Bowser (Santa)', 'Dry Bones (Gold)', 
+'Gold Koopa (Freerunning)', 'King Bob-omb (Gold)', 'King Boo (Gold)',
+'Mario (Hakama)', 'Mario (Tuxedo)', 
+'Peach (Vacation)', 'Pink Gold Peach', 'Rosalina (Swimwear)',
+'Shy Guy (Ninja)', 'Mario (Sunshine)', 
+'Boomerang Bro', 'Donkey Kong', 'Toad (Pit Crew)']
 
-# Drivers
-D = [ ws.cell(row=1, column=2+d).value for d in range(DRIVERS)]
-x = { d : m.addVar(vtype=GRB.BINARY) for d in D }
+###### LOAD DATA
 
-# Courses
-C = [ ws.cell(row=2+c, column=1).value for c in range(COURSES)]
+# Tickets and level
+cost = {
+'N':{'cost' : 800,
+     'requirements' : {0:40 + UNAQUIRED_DRIVERS_COST, 1:40, 2:38, 3:33, 4:25, 5:14, 6:0, 7:20}
+     },
+'S':{'cost' : 3000,
+     'requirements' : {0:15 + UNAQUIRED_DRIVERS_COST, 1:15, 2:14, 3:12, 4:9, 5:5, 6:0, 7:8}
+     },
+'H':{'cost' : 12000,
+     'requirements' : {0:9 + UNAQUIRED_DRIVERS_COST, 1:9, 2:8, 3:7, 4:5, 5:3, 6:0, 7:5}
+     }
+}
 
-# Subsets
-S = {}
-for i in range(COURSES):
-    s = []
-    for j in range(DRIVERS):
-        if ws.cell(row=2+i, column=2+j).value == 1:
-            s.append(D[j])
-    S[C[i]] = s
+wb = oxl.load_workbook('raw.xlsx')
+ws = wb['base']
 
-# Set constraints
-for course in C:
-    m.addConstr( quicksum( x[d] for d in S[course] ) >= 1)
+courses = set()
+drivers = set()
+coverage = {}
+investment = {}
+costs = {}
 
-m.setObjective( quicksum(x[d] for d in D), GRB.MINIMIZE)
-m.optimize()
+# Function that calculates a drivers cost to max out
 
-opt = m.objVal
-best = m.objVal
+def calculate_driver_cost(tier, driver, current_level, current_tickets):
+    
+    if( HIGH_ENDS_ONLY and (tier == 'H' or driver in PRIORITY_DRIVERS) ) or not HIGH_ENDS_ONLY:
+        calculated_cost = -1
+        
+        if current_level < 6:    
+            calculated_cost +=  cost[tier]['cost'] * ( cost[tier]['requirements'][current_level] - current_tickets )
+        
+        calculated_cost +=  cost[tier]['cost'] * (cost[tier]['requirements'][7]) if TARGET_LEVEL_7 and current_level < 7 else 0
+    
+        if ( ONLY_USE_ADQUIRED_DRIVERS and current_level >= 1 ) or not ONLY_USE_ADQUIRED_DRIVERS :
+            drivers.add(driver)
+            costs[driver] = calculated_cost if CONSIDER_CURRENT_INVESTMENT_LEVELS else 1
+        
+    
+# Load driver and iventory data    
+        
+row = 1
+while( ws.cell(row=row, column=5).value is not None ):
+    
+    tier = ws.cell(row=row, column=4).value
+    driver = ws.cell(row=row, column=5).value
+    current_level = ws.cell(row=row, column=6).value
+    current_tickets = ws.cell(row=row, column=7).value
+    
+    calculate_driver_cost(tier, driver, current_level, current_tickets)
 
-number = 1
-while opt == best:
+    row += 1
 
-    # Print solution
-    print(f'With these {opt} drivers: ({number})')
-    u = []
-    row = 1
-    for d in D:
-        if x[d].x == 1:
-            wb['solutions'].cell(row=row, column=number).value = d
-            u.append(d)
-            print(d)
-            row += 1
+# Read the course-driver coverage data
 
-    number += 1
-    opt = m.objVal
+row = 1
+while( ws.cell(row=row, column=1).value is not None ):
+    
+    driver = ws.cell(row=row, column=1).value.split(':')[0]
+    course = ws.cell(row=row, column=2).value
+    
+    if str(course)[0] not in ('0', '^') and driver in drivers:
+        
+        if course in courses:
+            coverage[course].add(driver)
+        else:
+            courses.add(course)
+            coverage[course] = set([driver])
+    
+    row+= 1
+    
+# Remove unused priority drivers
+prio_copy = set(PRIORITY_DRIVERS)
+for d in prio_copy:
+    if d not in drivers:
+        PRIORITY_DRIVERS.remove(d)
 
-    # Block solution
-    m.addConstr(quicksum(x[d] for d in u) <= len(u) - 1)
+# Minimum Drivers per Course
+courses_copy = set(courses)
+priority_courses = set()
+for course in courses_copy:
 
-    # Find new solution
-    m.update()
-    m.optimize()
+    # Remove courses without the minimum required drivers
+    if len(coverage[course]) < MINIMUM_DRIVERS_PER_COURSE:
+        courses.remove(course)
+        
+    # Calculate if a course has enough priority drivers
+    n = sum([ 1 if d in PRIORITY_DRIVERS else 0 for d in coverage[course] ])
+    if n >= MINIMUM_PRIORITY_DRIVERS_PER_COURSE:
+        priority_courses.add(course)
 
-wb.save('solution.xlsx')
+
+# Create the LP problem
+model = plp.LpProblem('MKT_Maximum_Coverage', plp.LpMinimize)
+
+chosen_drivers = {d : plp.LpVariable(f'{d}', lowBound=0, upBound=1, cat='Integer') for d in drivers}
+
+# objective function
+model += plp.lpSum( costs[d] * chosen_drivers[d] for d in drivers )
+
+# Constraint: Cover each course at least once
+for c in courses:
+    if not REQUIRE_PRIORITY_DRIVERS or c not in priority_courses:
+        model += plp.lpSum( chosen_drivers[d] for d in coverage[c] ) >= 1
+    elif REQUIRE_PRIORITY_DRIVERS and c in priority_courses:
+        model += plp.lpSum( chosen_drivers[d] for d in PRIORITY_DRIVERS if d in coverage[c] ) >= 1
+
+#model+= chosen_drivers['Pauline (Party Time)'] == 0
+#model+= chosen_drivers['Dry Bowser'] == 1
+
+# Solve the LP model
+model.solve()
+print( plp.LpStatus[model.status] )
+print( plp.value(model.objective))
+
+# Print the solution
+OPTIMAL_DRIVERS = set()
+for d in drivers:
+    if chosen_drivers[d].varValue == 1:
+        OPTIMAL_DRIVERS.add(d)
+        
+output = []
+for d in OPTIMAL_DRIVERS:
+    
+    if d == 'King Bob-Omb (Gold)':
+        x = 1
+    
+    unique_courses = set()
+    for c in courses:
+        if d in coverage[c]:
+            n = sum([ 1 if d2 in coverage[c] else 0 for d2 in OPTIMAL_DRIVERS ])
+            if n == 1:
+                unique_courses.add(c)
+            if d in PRIORITY_DRIVERS:
+                n = sum([ 1 if d2 in coverage[c] else 0 for d2 in OPTIMAL_DRIVERS.intersection(OPTIMAL_DRIVERS) ])
+                if n == 1:
+                    unique_courses.add(c)
+    x = 1
+    if len(unique_courses) > 0:
+        row = {'driver' : d, 'cost':costs[d]+1, 'unique_coverage': len(unique_courses),
+                'value': (costs[d]+1)/len(unique_courses),'unique_courses': str(unique_courses)}
+        output.append(row)
+    #print(f'{d} costs {costs[d]} to max out and covers {len(unique_courses)} unique courses: \n{unique_courses}')
+    
+ans_df = pd.DataFrame(output).sort_values(by=['value'])   
+ans_df.to_csv('drivers_unique.csv')
+            
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)  
